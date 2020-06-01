5b5a5a7d8edd7ec578e86f6de9c4ec48d4ea0d73
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-01 17:00:02 +0100

    kill previous process before starting a new one

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ program
         throw new Error(`Could not find the file ${name}`)
     }
 
+    let proc;
       const start = debounce(()=>{
+          if (proc) { 
+              proc.kill;
+              console.log('>>>>>> Starting proces...')
+            }
         spawn('node', [name], {stdio:'inherit'});
     }, 1000);
     
@@ -27,7 +32,7 @@ program
         .on('change', start)
         .on('unlink', start); 
      
-  });
+  }); 
  
 program.parse(process.argv);
 
