eb0eebf0c34b4da0a2342b4e086f898b3551a7a0
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-01 11:51:10 +0100

    run chokidar on exact events (add, change, unlink)

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
-console.log('lol');
 
 const chokidar = require('chokidar');
+let firstRun = true;
+if (firstRun) {
+    chokidar.watch('.')
+    .on('add', (event, path)=>{
+        console.log('file added ' + event)
+    })
+    .on('change', (event, path)=>{
+        console.log('file changed ' + event)
+    })
+    .on('unlink', (event, path)=>{
+        console.log('file unlinked ' + event)
+    }); 
 
-chokidar.watch('.').on('all', (event,path)=>{
-    console.log(event, path);
-});
+    firstRun = false;
+}
+
